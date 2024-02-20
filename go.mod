affcc255f93e93e145cb54e8b407e0022f3299f4
Tamal Saha <tamal@appscode.com>
2024-02-20 02:02:30 -0800

    wip
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go.bytebuilders.dev/smtprelay-auth-generator
 
 go 1.22.0
+
+require github.com/spf13/pflag v1.0.5
--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	flag "github.com/spf13/pflag"
+	"net/url"
+)
+
+func main() {
+	var passwd string
+	flag.StringVar(&passwd, "token", "", "Zepto mail token")
+
+	var u url.URL
+	u.Scheme = "starttls"
+	u.Host = "smtp.zeptomail.com:587"
+	u.User = url.UserPassword("emailapikey", passwd)
+	fmt.Println(u.String())
+}
