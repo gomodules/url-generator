b4d8d9e811f78d00c164343a560e8c30280e9443
Tamal Saha <tamal@appscode.com>
2024-02-23 22:21:48 -0800

    wip
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -9,17 +9,21 @@ import (
 
 func main() {
 	var (
-		scheme, host, user, passwd string
+		scheme, host, user, passwd, path, rawQuery string
 	)
 	flag.StringVarP(&scheme, "scheme", "s", "", "URL scheme")
 	flag.StringVarP(&host, "host", "h", "", "URL host")
 	flag.StringVarP(&passwd, "user", "u", "", "URL username")
 	flag.StringVarP(&passwd, "password", "p", "", "URL password")
+	flag.StringVar(&path, "path", "", "URL path")
+	flag.StringVarP(&rawQuery, "query", "q", "", "URL query string")
 	flag.Parse()
 
 	var u url.URL
 	u.Scheme = scheme
 	u.Host = host
 	u.User = url.UserPassword(user, passwd)
+	u.Path = path
+	u.RawQuery = rawQuery
 	fmt.Println(u.String())
 }
